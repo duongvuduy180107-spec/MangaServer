d7f229988a552b26332e86027d31702515588baf
duongvuduy180107-spec <duongvuduy180107@gmail.com>
2026-07-26 03:03:49 +0900

    github.js の更新

--- github.js
+++ github.js
@@ -1,3 +1,13 @@
 const axios = require("axios");
 
-module.exports = {};
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const GITHUB_OWNER = process.env.GITHUB_OWNER;
+const GITHUB_REPO = process.env.GITHUB_REPO;
+const GITHUB_BRANCH = process.env.GITHUB_BRANCH || "main";
+
+module.exports = {
+    GITHUB_TOKEN,
+    GITHUB_OWNER,
+    GITHUB_REPO,
+    GITHUB_BRANCH
+};
